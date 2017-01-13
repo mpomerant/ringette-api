2e3ae69d688670ee92b01384e1b80ffc4e2e513f
MARK_POMERANT <mark.pomerant@oracle.com>
2017-01-13 11:51:48 -0500

    add standings to app locals

--- routes/api.js
+++ routes/api.js
@@ -1,40 +1,60 @@
 var express = require('express');
 var game = require('./games');
+var app = require('../app');
 var team = require('./team');
 var TeamModel = require('../models/team');
+var GameModel = require('../models/game');
 var standings = require('./standings');
 var router = express.Router();
+var StandingsHelper = require('./standing');
 
-router.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  //res.setHeader('Access-Control-Allow-Headers', 'headers_you_want_to_accept');
-  next();
-});
-var reduceTeams = function(result, team) {
-
-  result[team.name] = team._id;
-
-  return result;
-}
-
-var reduceAssociations = function(result, team) {
-  if (!this[team.association]) {
-    this[team.association] = 1;
-    result.push(team.association);
-
-  }
-  return result;
-}
-router.use(function(req, res, next) {
-  var allTeams = TeamModel.find().exec().then(function(teams) {
-    req.teams = teams;
-
-
-    req.teamMap = teams.reduce(reduceTeams, {});
-    var test = {};
-    req.associations = teams.reduce(reduceAssociations.bind(test), [])
+router.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Headers', 'headers_you_want_to_accept');
     next();
-  });
+});
+
+
+var reduceTeams = function (result, team) {
+
+    result[team.name] = team._id;
+
+    return result;
+}
+
+var reduceAssociations = function (result, team) {
+    if (!this[team.association]) {
+        this[team.association] = 1;
+        result.push(team.association);
+
+    }
+    return result;
+}
+router.use(function (req, res, next) {
+    var allTeams = TeamModel.find().exec().then(function (teams) {
+        req.teams = teams;
+
+
+        req.teamMap = teams.reduce(reduceTeams, {});
+        var test = {};
+        req.associations = teams.reduce(reduceAssociations.bind(test), [])
+        next();
+    });
+
+});
+
+router.use(function (req, res, next) {
+
+    if (!app.locals.standings) {
+        StandingsHelper.getStandings(req.teams).then(function (standings) {
+            app.locals.standings = standings;
+            next();
+        });
+
+    } else {
+        next();
+    }
+
 
 });
 
